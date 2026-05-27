bbcd5c58a0c3b64f3637e8f474519bc088b8de05
Chendraya Santhosh V <94157072+Santhosh24082003@users.noreply.github.com>
2026-05-27 19:24:44 +0530

    fix some issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import AppRouter from './router'
+import { AuthProvider } from './context/AuthContext'
+
 function App() {
-  return null
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  )
 }
 
 export default App
